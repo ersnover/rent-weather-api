01df22481a815f1449f073b3ec1d2793fd9b42fd
Eric Snover <ersnover@gmail.com>
2019-10-07 23:39:33 -0500

    uncommented city search route

--- routes/rent.js
+++ routes/rent.js
@@ -26,23 +26,23 @@ router.get('/', [
     .catch(() => res.json({errors: 'No cities matched your rent criteria'}))
 })
 
-// router.get('/:city', [
-//     check('city', 'Invalid city name').isString(),
-//     check('key', 'Invalid key value').equals(process.env.MY_KEY)
-// ], (req,res) => {
+router.get('/:city', [
+    check('city', 'Invalid city name').isString(),
+    check('key', 'Invalid key value').equals(process.env.MY_KEY)
+], (req,res) => {
 
-//     const errors = validationResult(req)
-//     if (!errors.isEmpty()) {
-//         return res.status(400).json({errors: errors.array()})
-//     }
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
 
-//     let city = req.params.city
+    let city = req.params.city
 
-//     // leaving this .any() to account for multiple cities by the same name e.g. Springfield
-//     db.any(`SELECT city, state, rent FROM cities WHERE LOWER(city) = LOWER($1)`, [city])
-//     .then(cities => cities[0] ? res.json(cities) : res.json({errors: 'No cities matched your search criteria'}))
-//     .catch(() => res.json({error: 'No cities matched your criteria'}))
-// })
+    // leaving this .any() to account for multiple cities by the same name e.g. Springfield
+    db.any(`SELECT city, state, rent FROM cities WHERE LOWER(city) = LOWER($1)`, [city])
+    .then(cities => cities[0] ? res.json(cities) : res.json({errors: 'No cities matched your search criteria'}))
+    .catch(() => res.json({error: 'No cities matched your criteria'}))
+})
 
 
 module.exports = router
